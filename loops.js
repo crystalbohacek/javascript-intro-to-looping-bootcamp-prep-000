9cc5e811640eb038f667baca7c83b445e8f8dc6e
Crystal Bohacek <crystalbohacek@gmail.com>
2018-10-22 15:01:37 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,9 @@
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+}
+
 function doWhileLoop(array){
   while(incrementVariable() && array.length > 0){
       array.length = (array.length-1)
